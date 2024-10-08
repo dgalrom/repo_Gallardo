57e9e6cc36cdcef90095cf46acf8d447e2552fab
Diego Gallardo <dgalrom707@g.educaand.es>
2024-10-08 17:31:08 +0200

    Clase 08/10

---
+++ programacion/proyecto_programacion/src/ud1/ud1_boletin_ejercicios/ej1.java
@@ -0,0 +1,10 @@
+package ud1.ud1_boletin_ejercicios;
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- programacion/proyecto_programacion/src/ud2/TablaMultiplicar.java
+++ programacion/proyecto_programacion/src/ud2/TablaMultiplicar.java
@@ -10,16 +10,7 @@ public class TablaMultiplicar {
 	int numero = scan.nextInt();
 scan.close();
 System.out.println(numero * 0);
-System.out.println(numero * 1);
-System.out.println(numero * 2);
-System.out.println(numero * 3);
-System.out.println(numero * 4);
-System.out.println(numero * 5);
-System.out.println(numero * 6);
-System.out.println(numero * 7);
-System.out.println(numero * 8);
-System.out.println(numero * 9);
-System.out.println(numero * 10);
+
 }
 }
 //corregir de repo juanfran
