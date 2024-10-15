805469a2ac53e2e65c364d60e8498f0dada065f9
dgalrom <dgalrom707@g.educaand.es>
2024-10-15 17:32:12 +0200

    Add files via upload

---
+++ programacion/proyecto_programacion/src/ud2_boletin_ejercicios/ej15.java
@@ -0,0 +1,54 @@
+package ud2.ud2_boletin_ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * Pide por teclado un dia de la semana y que diga que asignatura
+ * toca a primera hora de dicho dia
+ */
+
+public class ej15 {
+
+	public static void main(String[] args) {
+		//Scanner e instrucciones
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Introduzca un dia de la semana");
+		System.out.println("Se le devolvera la asignatura que toca a primera hora");
+		String diaU = scan.nextLine();
+		
+		diaU = diaU.toLowerCase();
+		final String LUN = "lunes";
+		final String MAR = "martes";
+		final String MIE = "miercoles";
+		final String JUE = "jueves";
+		final String VIE = "viernes";
+
+		//Situaciones
+		switch (diaU) {
+		case LUN:
+			System.out.println("Entornos de desarrollo");
+			break;		
+			
+		case MAR:
+			System.out.println("Programacion");
+			break;
+
+		case MIE:
+			System.out.println("Bases de datos");
+			break;
+			
+		case JUE:
+			System.out.println("Sistemas");
+			break;
+			
+		case VIE:
+			System.out.println("Fol");
+			break;
+
+		default: System.out.println("Introduzca un dia valido");
+			break;
+		}
+
+	}
+
+}
---
+++ programacion/proyecto_programacion/src/ud2_boletin_ejercicios/ej20.java
@@ -0,0 +1,18 @@
+package ud2.ud2_boletin_ejercicios;
+
+public class ej20 {
+
+	public static void main(String[] args) {
+
+	int n2 = 1;
+	int n3 = 1;
+		for(int n1=0 ; n1>=0 ; n1=n2+n3 ) {
+
+			System.out.println(n1);
+			n2=n3;
+			n1=n2;
+			
+	};
+	
+	}
+}
