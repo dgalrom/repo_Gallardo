8f5e801235156f11611ff296d8db57dd8e8de178
Diego Gallardo <grdiego2646@gmail.com>
2024-11-14 16:54:06 +0100

    ..

---
+++ programacion/tarea2usoObjetos/src/com/diegoGallardo/tarea2/UsoDeObjetos.java
@@ -0,0 +1,9 @@
+package com.diegoGallardo.tarea2;
+
+public class UsoDeObjetos {
+
+	public static void main(String[] args) {
+			
+	}
+
+}
---
+++ programacion/tarea2usoObjetos/src/com/diegoGallardo/tarea2/UtilidadesMatematicas.java
@@ -0,0 +1,48 @@
+package com.diegoGallardo.tarea2;
+
+public class UtilidadesMatematicas {
+		
+	public static int posicionDeDigito(int posicion) {
+		
+		boolean localizado = false;
+		
+		/**
+		 * -localizar primera ocurrencia de un dígito entero-,
+		 * si no se localiza, devuelve -1 (hecho)
+		 */
+		
+		//numero de cifras
+		int numero;
+		int numCifras = 1;
+		while (numero / 10 != 0) {
+			numCifras++;
+			numero = numero / 10;
+		}
+		
+		int posicionLectura;
+		
+		for( posicionLectura=0 ; posicionLectura==numCifras ; ) {
+				
+		while(numero%10)
+			
+		}
+		
+		
+		if(!localizado) {
+			
+		}else if(localizado) {
+			posicion = -1;
+			
+		}
+		
+	}
+
+	public static int trozoDeNumero(int trozo) {
+		
+		
+		
+	}
+
+	
+	
+}
