a58c00baaa4553f3d3bbd3219d376513f0dc7b4d
dgalrom <dgalrom707@g.educaand.es>
2024-11-17 18:38:07 +0100

    Avance de práctica

--- programacion/tarea2usoObjetos/src/com/diegoGallardo/UsoDeObjetos.java
+++ programacion/tarea2usoObjetos/src/com/diegoGallardo/UsoDeObjetos.java
@@ -3,7 +3,9 @@ package com.diegoGallardo;
 public class UsoDeObjetos {
 
 	public static void main(String[] args) {
-			
+		long num=1020;
+		int dig=2;
+		System.out.println("posicionDeDigito es "+ UtilidadesMatematicas.posicionDeDigito(num, dig));
+		
 	}
-
 }
--- programacion/tarea2usoObjetos/src/com/diegoGallardo/UtilidadesMatematicas.java
+++ programacion/tarea2usoObjetos/src/com/diegoGallardo/UtilidadesMatematicas.java
@@ -1,5 +1,27 @@
 package com.diegoGallardo;
 
 public class UtilidadesMatematicas {
+	
+	public static int numCifras(long numero) {
+        int numCifras = 1;
+        while (numero / 10 != 0) {
+            numCifras++;
+            numero = numero / 10;
+        }
+        return numCifras;
+    }
+	
+	
+	public static int posicionDeDigito (long num, int dig) {	
+	
+	int pos= numCifras(num);
+	int cif = 0;
+	
+	for( int act=(int) num ; cif==dig ; pos-- ) {
+		cif = act%10;
+		act = act/10;
+	}
+	return pos;
 		
+}
 }
